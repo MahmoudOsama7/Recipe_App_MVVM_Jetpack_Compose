1bbe75c657ce0a098a92699e1f1071b1fab35c5e
Mahmoud Osama <mahmoud@34ml.com>
2023-05-23 11:57:39 +0200

    project fix and adding compose

---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.codingwithmitch.mvvmrecipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.main_container,RecipeListFragment()).commit()
+    }
+}
