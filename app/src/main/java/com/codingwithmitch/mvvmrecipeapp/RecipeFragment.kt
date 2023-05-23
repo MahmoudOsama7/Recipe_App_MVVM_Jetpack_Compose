0cc2bcabe01ceeb60440c14e58a116ef903d16d5
Mahmoud Osama <mahmoud@34ml.com>
2023-05-23 13:48:24 +0200

    adding navigation

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/RecipeFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/RecipeFragment.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.clickable
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RecipeListFragment:Fragment() {
+class RecipeFragment:Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +18,7 @@ class RecipeListFragment:Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text("He", modifier = Modifier.clickable { findNavController().navigate(R.id.recipeFragment) })
+
             }
         }
     }
